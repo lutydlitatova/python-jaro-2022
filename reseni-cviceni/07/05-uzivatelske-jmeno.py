1f108154039937d64505411a3f2323e20fdf82e5
Ludmila Tydlitatova <ludmila.tydlitatova@gmail.com>
2022-04-12 19:49:36 +0200

    Reseni 07

--- reseni-cviceni/07/05-uzivatelske-jmeno.py
+++ reseni-cviceni/07/05-uzivatelske-jmeno.py
@@ -2,8 +2,8 @@ import re
 
 uzivatelske_jmeno = input("Zadejte uzivatelske jmeno: ")
 
-uzviatelske_jmeno_re = re.compile("\w{1,8}")
-if uzviatelske_jmeno_re.fullmatch(uzivatelske_jmeno):
+uzivatelske_jmeno_re = re.compile("\w{1,8}")  # Nebo ještě přesněji, re.compile("[a-z]{1,8}")
+if uzivatelske_jmeno_re.fullmatch(uzivatelske_jmeno):
     print("Uživatelské jméno je v pořádku.")
 else:
     print("Uživatelské jméno smí obsahovat pouze malá písmena a smí být maximálně 8 znaků dlouhé.")
